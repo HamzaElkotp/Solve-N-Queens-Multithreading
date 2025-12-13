1710425c003310be3e1880d10fdbf05d9c4a2ec9
sara400m <sarah_20230249@fci.helwan.edu.eg>
2025-12-13 20:52:05 +0200

    Snapshot.java

--- src/model/Snapshot.java
+++ src/model/Snapshot.java
@@ -1,14 +1,23 @@
 package model;
 
 public class Snapshot {
-    public Snapshot(int threadId, int[] queens, long timestamp) {}
+
+    private final int threadId;
+    private final int[] queens;
+    private final long timestamp;
+    
+    public Snapshot(int threadId, int[] queens, long timestamp) {
+        this.threadId = threadId;
+        this.queens = queens.clone();
+        this.timestamp = timestamp;
+    }
     public int getThreadId() {
-        return 0;
+        return threadId;
     }
     public int[] getQueens() {
-        return null;
+        return queens.clone();
     }
     public long getTimestamp() {
-        return 0;
+        return timestamp;
     }
 }
