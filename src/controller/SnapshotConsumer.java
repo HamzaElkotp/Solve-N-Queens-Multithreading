b4b19b004e4d982461d3ca4fb26a3ed80b88d6c3
HamzaDev <hmzt25235@gmail.com>
2025-12-14 07:37:50 +0200

    finish SnapshotConsumer

--- src/controller/SnapshotConsumer.java
+++ src/controller/SnapshotConsumer.java
@@ -3,15 +3,58 @@ package controller;
 import model.Snapshot;
 import ui.BoardPanel;
 
+import javax.swing.*;
 import java.awt.*;
+import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SnapshotConsumer {
 
-    public SnapshotConsumer(BoardPanel boardPanel,
-                            java.util.concurrent.BlockingQueue<Snapshot> queue,
-                            java.util.Map<Integer, Color> threadColors) {}
+    public BoardPanel boardPanel;
+    public BlockingQueue<Snapshot> queue;
+    public HashMap<Integer, Color> threadColors;
+    public AtomicBoolean is_Running;
+    public HashMap latestStates;
 
-    public void startConsuming() {}
+    private Thread consumerThread;
 
-    public void stopConsuming() {}
+    public SnapshotConsumer(BoardPanel boardPanel, BlockingQueue<Snapshot> queue, HashMap<Integer, Color> threadColors) {
+        this.boardPanel = boardPanel;
+        this.queue = queue;
+        this.threadColors = threadColors;
+        this.is_Running = new AtomicBoolean(false);
+        latestStates = new HashMap<Integer, Snapshot>();
+    }
+
+    public void startConsuming() {
+        this.is_Running.set(true);
+
+        consumerThread = new Thread(() -> {
+            try{
+                while(is_Running.get()) {
+                    Snapshot snapshot = queue.take(); // BLOCKING -> no busy waiting
+                    latestStates.put(snapshot.getThreadId(), snapshot);
+
+                    SwingUtilities.invokeLater(() -> {
+                        boardPanel.updateBoard(latestStates, threadColors);
+                    });
+                }
+            } catch(InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+
+        consumerThread.start();
+    }
+
+    public void stopConsuming() {
+        this.is_Running.set(false);
+
+        if (consumerThread != null) {
+            consumerThread.interrupt();
+        }
+
+        latestStates.clear();
+    }
 }
